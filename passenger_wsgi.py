7d188ba87dc16ac10e71f4d7227c3df5e7696532
James Moore <james@panic.com>
2017-11-10 16:08:57 -0800

    simplify passenger file

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,10 +1,10 @@
 import sys, os
 from app import app as application
 
-VENV = '/var/www/moa/.moa-venv'
-PYTHON_BIN = VENV + '/bin/python3'
-
-if sys.executable != PYTHON_BIN:
-    os.execl(PYTHON_BIN, PYTHON_BIN, *sys.argv)
-
-sys.path.insert(0, VENV + '/lib/python3.6/site-packages')
+# VENV = '/var/www/moa/.moa-venv'
+# PYTHON_BIN = VENV + '/bin/python3'
+#
+# if sys.executable != PYTHON_BIN:
+#     os.execl(PYTHON_BIN, PYTHON_BIN, *sys.argv)
+#
+# sys.path.insert(0, VENV + '/lib/python3.6/site-packages')
