47f7b6d6f668d94238e58e89e4606531310055d6
James Moore <james@panic.com>
2017-11-11 20:42:35 -0800

    better failure tracking during sending

--- moa/helpers.py
+++ moa/helpers.py
@@ -28,17 +28,17 @@ def send_tweet(tweet, reply_to, media_ids, twitter_api):
                                               media=media_ids,
                                               in_reply_to_status_id=reply_to,
                                               verify_status_length=False).id
+            post_success = True
+
         except TwitterError as e:
             logger.error(e.message)
 
             if e.message[0]['code'] == 187:
                 # Status is a duplicate
-                return
+                return None
             if retry_counter < TWITTER_RETRIES:
                 retry_counter += 1
                 time.sleep(TWITTER_RETRY_DELAY)
-
-        post_success = True
 
     if retry_counter == TWITTER_RETRIES:
         logger.error("Retry limit reached.")
@@ -66,6 +66,7 @@ def send_toot(tweet, settings, mast_api, reply_to=None):
                 in_reply_to_id=reply_to)
 
             reply_to = post["id"]
+            post_success = True
 
         except MastodonAPIError as e:
             logger.error(e)
@@ -73,8 +74,6 @@ def send_toot(tweet, settings, mast_api, reply_to=None):
             if retry_counter < MASTODON_RETRIES:
                 retry_counter += 1
                 time.sleep(MASTODON_RETRY_DELAY)
-
-        post_success = True
 
     if retry_counter == MASTODON_RETRIES:
         logger.error("Retry limit reached.")
