1d714127ab809a403af117d1dd2e119778fc89fb
James Moore <james@panic.com>
2017-11-11 20:41:56 -0800

    improved last id tracking

--- moa/worker.py
+++ moa/worker.py
@@ -62,8 +62,8 @@ for bridge in bridges:
             since_id=bridge.mastodon_last_id
         )
         if len(new_toots) != 0:
-            mastodon_last_id = int(new_toots[0]['id'])
-            l.info(f"Mastodon: {bridge.mastodon_user} -> Twitter: {bridge.twitter_handle}")
+            l.info(f"Mastodon: {bridge.mastodon_user} {mastodon_last_id} -> Twitter: {bridge.twitter_handle}")
+            # mastodon_last_id = int(new_toots[0]['id'])
             l.info(f"{len(new_toots)} new toots found")
 
     if bridge.settings.post_to_mastodon:
@@ -72,143 +72,139 @@ for bridge in bridges:
             include_rts=True,
             exclude_replies=False)
         if len(new_tweets) != 0:
-            twitter_last_id = new_tweets[0].id
-            l.info(f"Twitter: {bridge.twitter_handle} -> Mastodon: {bridge.mastodon_user}")
+            l.info(f"Twitter: {bridge.twitter_handle} {twitter_last_id} -> Mastodon: {bridge.mastodon_user}")
+            # twitter_last_id = new_tweets[0].id
             l.info(f"{len(new_tweets)} new tweets found")
 
-    if bridge.settings.post_to_twitter:
-        if len(new_toots) != 0:
-            new_toots.reverse()
+    if bridge.settings.post_to_twitter and len(new_toots) != 0:
+        new_toots.reverse()
 
-            url_length = max(twitter_api.GetShortUrlLength(False), twitter_api.GetShortUrlLength(True)) + 1
-            l.debug(f"URL length: {url_length}")
+        url_length = max(twitter_api.GetShortUrlLength(False), twitter_api.GetShortUrlLength(True)) + 1
+        l.debug(f"URL length: {url_length}")
 
-            for toot in new_toots:
+        for toot in new_toots:
 
-                t = Toot(toot, bridge.settings)
-                t.url_length = url_length
+            t = Toot(toot, bridge.settings)
+            t.url_length = url_length
 
-                l.info(f"Working on toot {t.id}")
+            l.info(f"Working on toot {t.id}")
 
-                # l.debug(pp.pformat(toot))
+            # l.debug(pp.pformat(toot))
 
-                if t.should_skip:
-                    continue
+            if t.should_skip:
+                continue
 
-                t.split_toot()
+            t.split_toot()
 
+            if c.SEND:
+                t.download_attachments()
+
+            reply_to = None
+            media_ids = []
+
+            # Upload our attachments for the last tweet
+            for attachment in t.attachments:
+
+                file = attachment[0]
+                description = attachment[1]
+
+                temp_file_read = open(file, 'rb')
+                l.info(f'Uploading {description} {file}')
+                media_id = twitter_api.UploadMediaChunked(media=temp_file_read)
+
+                if description:
+                    twitter_api.PostMediaMetadata(media_id, alt_text=description)
+
+                media_ids.append(media_id)
+                temp_file_read.close()
+                os.unlink(file)
+
+            if t.is_self_reply:
+
+                # In the case where a toot has been broken into multiple tweets
+                # we want the last one posted
+                mapping = session.query(Mapping).filter_by(mastodon_id=t.in_reply_to_id).order_by(Mapping.created.desc()).first()
+
+                if mapping:
+                    reply_to = mapping.twitter_id
+                    l.info(f"Replying to twitter status {reply_to} / masto status {t.in_reply_to_id}")
+
+            # Do normal posting for all but the last tweet where we need to upload media
+            for status in t.tweet_parts[:-1]:
                 if c.SEND:
-                    t.download_attachments()
+                    reply_to = send_tweet(status, reply_to, None, twitter_api)
 
-                reply_to = None
-                media_ids = []
-
-                # Upload our attachments for the last tweet
-                for attachment in t.attachments:
-
-                    file = attachment[0]
-                    description = attachment[1]
-
-                    temp_file_read = open(file, 'rb')
-                    l.info(f'Uploading {description} {file}')
-                    media_id = twitter_api.UploadMediaChunked(media=temp_file_read)
-
-                    if description:
-                        twitter_api.PostMediaMetadata(media_id, alt_text=description)
-
-                    media_ids.append(media_id)
-                    temp_file_read.close()
-                    os.unlink(file)
-
-                if t.is_self_reply:
-
-                    # In the case where a toot has been broken into multiple tweets
-                    # we want the last one posted
-                    mapping = session.query(Mapping).filter_by(mastodon_id=t.in_reply_to_id).order_by(Mapping.created.desc()).first()
-
-                    if mapping:
-                        reply_to = mapping.twitter_id
-                        l.info(f"Replying to twitter status {reply_to} / masto status {t.in_reply_to_id}")
-
-                # Do normal posting for all but the last tweet where we need to upload media
-                for status in t.tweet_parts[:-1]:
-                    if c.SEND:
-                        reply_to = send_tweet(status, reply_to, None, twitter_api)
-
-                        if reply_to != 0:
-                            m = Mapping()
-                            m.mastodon_id = t.id
-                            m.twitter_id = reply_to
-                            session.add(m)
-
-                status = t.tweet_parts[-1]
-
-                if c.SEND:
-                    reply_to = send_tweet(status, reply_to, media_ids, twitter_api)
-
-                    if reply_to != 0:
+                    if reply_to:
                         m = Mapping()
                         m.mastodon_id = t.id
                         m.twitter_id = reply_to
                         session.add(m)
 
-                twitter_last_id = reply_to
+                        bridge.mastodon_last_id = t.id
 
-        bridge.mastodon_last_id = mastodon_last_id
-        bridge.twitter_last_id = twitter_last_id
+                        session.commit()
 
-        if c.SEND:
-            session.commit()
-
-    if bridge.settings.post_to_mastodon:
-
-        if len(new_tweets) != 0:
-
-            new_tweets.reverse()
-
-            for status in new_tweets:
-
-                l.info(f"Working on tweet {status.id}")
-
-                # l.debug(pp.pformat(status.__dict__))
-
-                tweet = Tweet(status, bridge.settings, twitter_api, mast_api)
-
-                l.debug(tweet.clean_content)
-
-                if tweet.should_skip:
-                    continue
-
-                if c.SEND:
-                    tweet.transfer_attachments()
-
-                twitter_last_id = status.id
-
-                reply_to = None
-                if tweet.is_self_reply:
-                    mapping = session.query(Mapping).filter_by(twitter_id=status.in_reply_to_status_id).first()
-
-                    if mapping:
-                        reply_to = mapping.mastodon_id
-                        l.info(f"Replying to mastodon status {reply_to}")
-
-                if c.SEND:
-                    mastodon_last_id = send_toot(tweet,
-                                                 bridge.settings,
-                                                 mast_api,
-                                                 reply_to=reply_to)
-
-                    if mastodon_last_id != 0:
-                        m = Mapping()
-                        m.mastodon_id = mastodon_last_id
-                        m.twitter_id = status.id
-                        session.add(m)
-
-            bridge.mastodon_last_id = mastodon_last_id
-            bridge.twitter_last_id = twitter_last_id
+            status = t.tweet_parts[-1]
 
             if c.SEND:
+                twitter_last_id = send_tweet(status, reply_to, media_ids, twitter_api)
+
+                if twitter_last_id:
+                    m = Mapping()
+                    m.mastodon_id = t.id
+                    m.twitter_id = reply_to
+                    session.add(m)
+
+                    bridge.twitter_last_id = twitter_last_id
+
+                bridge.mastodon_last_id = t.id
                 session.commit()
+
+    if bridge.settings.post_to_mastodon and len(new_tweets) != 0:
+
+        new_tweets.reverse()
+
+        for status in new_tweets:
+
+            l.info(f"Working on tweet {status.id}")
+
+            # l.debug(pp.pformat(status.__dict__))
+
+            tweet = Tweet(status, bridge.settings, twitter_api, mast_api)
+
+            # l.debug(tweet.clean_content)
+
+            if tweet.should_skip:
+                continue
+
+            if c.SEND:
+                tweet.transfer_attachments()
+
+            reply_to = None
+            if tweet.is_self_reply:
+                mapping = session.query(Mapping).filter_by(twitter_id=status.in_reply_to_status_id).first()
+
+                if mapping:
+                    reply_to = mapping.mastodon_id
+                    l.info(f"Replying to mastodon status {reply_to}")
+
+            if c.SEND:
+                mastodon_last_id = send_toot(tweet,
+                                             bridge.settings,
+                                             mast_api,
+                                             reply_to=reply_to)
+
+                if mastodon_last_id:
+                    m = Mapping()
+                    m.mastodon_id = mastodon_last_id
+                    m.twitter_id = status.id
+                    session.add(m)
+
+                    bridge.mastodon_last_id = mastodon_last_id
+
+                bridge.twitter_last_id = status.id
+                session.commit()
+
 
 session.close()
 l.info("All done")
