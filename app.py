8f376f955ffb9e1ddf34319e5430d057410dfd5b
James Moore <james@panic.com>
2017-11-13 22:27:25 -0800

    404 handler

--- app.py
+++ app.py
@@ -310,6 +310,11 @@ def logout():
     return redirect(url_for('index'))
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
 
     app.run()
