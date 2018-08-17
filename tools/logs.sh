f9cf7dbaf58eb61eed75d42eb2b74f11cfe45e44
James Moore <james@panic.com>
2018-08-17 10:00:54 -0700

    wrong labels

--- tools/logs.sh
+++ tools/logs.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Made with "Get you a multitail"
-# https://getyouamultitail.link/?d=N4IgbiBcCMA0IBsoG1QIIYCMCmTImhHgGMB7BUgJyhEuwBMiQAzASwWxoHox1KuA7kK4BbUui4UA5gGdBVANbZKAfWgA6aUzIiRdZjQAMIAL6w0WXDUIlyVGlLrYAdkzYduvfkIGjxk0ll5SiVVACZNQO1SXX0jU3NESzwCaIpqfABPXAoBN3ZOfB4+QWExCWk5AUVlFQBmSKlo2OwDfGMzCxwUmxAydJpMBABXTnh3QpBi7zL-SuDQlQAWRua9VvjOpO7rNPt8EXQpFwAXdHyPIq9S33KAoOqQ2oBWVdsWtpAOgF14emwZMQaABZcQAAkeoTBlSYYAAFlBmOgEDJsPARJsgA
+# https://getyouamultitail.link/?d=N4IgbiBcCMA0IBsoG1QIIYCMCmTImhHgGMB7BUgJyhEuwBMiQAzASwWxoHox1KuA7kK4BbUui4UA5gGdBVANbZKAfWgA6aUzIiRdZjQAMIAL6w0WXDQBM28lRpS62AHZM2Hbr35CBo8ZKksvKUSqrWmkF2uvpGpuaIlnggAMx2FNT4AJ64FALu7Jz4PHyCwmIS0nICisoqKZFS0XrYBvjGZhY4yQAs6Q74mAgArpzwHkUgJT7lAVUhYSo9jc2x7fFdVvgArP2ZICLoUq4ALugFnsXeZX4VgcE1oXXbKySkMa1xJgC68PTYMmINAAsuIAASPMJgqpMMAACygzHQCBk2HgIi+QA
 
 # Moa worker logs
 
-multitail -m 0 --label '[1] ' -ci red -I /var/www/moa/logs/worker_1.log --label '[1] ' -ci green -I /var/www/moa/logs/worker_2.log --label '[1] ' -ci yellow -I /var/www/moa/logs/worker_3.log --label '[1] ' -ci blue -I /var/www/moa/logs/worker_4.log --label '[1] ' -ci magenta -I /var/www/moa/logs/worker_5.log
+multitail -m 0 --label '[1] ' -ci red -I /var/www/moa/logs/worker_1.log --label '[2] ' -ci green -I /var/www/moa/logs/worker_2.log --label '[3] ' -ci yellow -I /var/www/moa/logs/worker_3.log --label '[4] ' -ci blue -I /var/www/moa/logs/worker_4.log --label '[5] ' -ci magenta -I /var/www/moa/logs/worker_5.log 
