5b819f4056de4cf2b2a1ebe4e5019688b45a7380
James Moore <james@panic.com>
2019-01-04 13:00:30 -0800

    migrate to pipenv

--- worker.sh
+++ worker.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 cd /var/www/moa
-source .moa-venv/bin/activate
 
 export MOA_CONFIG=ProductionConfig
-python -m moa.worker >> logs/worker.log 2>&1
+pipenv run python -m moa.worker >> logs/worker.log 2>&1
