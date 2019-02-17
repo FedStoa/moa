58a260e1877deffc24f6b93234dbe8c33bfa60bc
James Moore <james@panic.com>
2019-02-17 09:16:08 +0800

    add base62 functions

--- moa/helpers.py
+++ moa/helpers.py
@@ -53,7 +53,6 @@ def email_bridge_details(app, bridge):
 
 
 def send_blacklisted_email(app, username):
-
     if app.config.get('MAIL_SERVER', None):
         mail = Mail(app)
         body = render_template('access_denied.txt.j2', user=f"https://twitter.com/{username}")
@@ -66,3 +65,45 @@ def send_blacklisted_email(app, username):
 
         except Exception as e:
             app.logger.error(e)
+
+
+BASE62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# Taken from https://stackoverflow.com/questions/1119722/base-62-conversion
+
+
+def b62_encode(num, alphabet=BASE62):
+    """Encode a positive number in Base X
+
+    Arguments:
+    - `num`: The number to encode
+    - `alphabet`: The alphabet to use for encoding
+    """
+    if num == 0:
+        return alphabet[0]
+    arr = []
+    base = len(alphabet)
+    while num:
+        num, rem = divmod(num, base)
+        arr.append(alphabet[rem])
+    arr.reverse()
+    return ''.join(arr)
+
+
+def b62_decode(string, alphabet=BASE62):
+    """Decode a Base X encoded string into the number
+
+    Arguments:
+    - `string`: The encoded string
+    - `alphabet`: The alphabet to use for encoding
+    """
+    base = len(alphabet)
+    strlen = len(string)
+    num = 0
+
+    idx = 0
+    for char in string:
+        power = (strlen - (idx + 1))
+        num += alphabet.index(char) * (base ** power)
+        idx += 1
+
+    return num
