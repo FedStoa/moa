b96321f40d17ad89f8a65c64463d3e388271f048
James Moore <james@panic.com>
2019-12-01 17:37:09 -0800

    always update md

--- moa/update_metadata.py
+++ moa/update_metadata.py
@@ -55,54 +55,54 @@ for bridge in bridges:
     if not bridge.md:
         bridge.md = BridgeMetadata()
 
-        if bridge.mastodon_access_code:
-            mastodonhost = bridge.mastodon_host
+    if bridge.mastodon_access_code:
+        mastodonhost = bridge.mastodon_host
 
-            mast_api = Mastodon(
-                    client_id=mastodonhost.client_id,
-                    client_secret=mastodonhost.client_secret,
-                    api_base_url=f"https://{mastodonhost.hostname}",
-                    access_token=bridge.mastodon_access_code,
-                    debug_requests=False,
-                    request_timeout=15,
-                    ratelimit_method='throw'
+        mast_api = Mastodon(
+                client_id=mastodonhost.client_id,
+                client_secret=mastodonhost.client_secret,
+                api_base_url=f"https://{mastodonhost.hostname}",
+                access_token=bridge.mastodon_access_code,
+                debug_requests=False,
+                request_timeout=15,
+                ratelimit_method='throw'
+        )
+
+        try:
+            profile = mast_api.account_verify_credentials()
+            bridge.md.is_bot = profile['bot']
+
+            statuses = mast_api.account_statuses(bridge.mastodon_account_id)
+            if len(statuses) > 0:
+                bridge.md.last_toot = statuses[0]["created_at"]
+
+        except (MastodonAPIError, MastodonNetworkError) as e:
+            l.error(e)
+            session.commit()
+            continue
+
+    if bridge.twitter_oauth_token:
+
+        try:
+            twitter_api = twitter.Api(
+                    consumer_key=c.TWITTER_CONSUMER_KEY,
+                    consumer_secret=c.TWITTER_CONSUMER_SECRET,
+                    access_token_key=bridge.twitter_oauth_token,
+                    access_token_secret=bridge.twitter_oauth_secret,
+                    tweet_mode='extended'  # Allow tweets longer than 140 raw characters
             )
+            tl = twitter_api.GetUserTimeline()
+        except TwitterError as e:
+            l.error(e)
+            session.commit()
+            continue
 
-            try:
-                profile = mast_api.account_verify_credentials()
-                bridge.md.is_bot = profile['bot']
+        else:
+            if len(tl) > 0:
+                d = datetime.strptime(tl[0].created_at, '%a %b %d %H:%M:%S %z %Y')
+                bridge.md.last_tweet = d
 
-                statuses = mast_api.account_statuses(bridge.mastodon_account_id)
-                if len(statuses) > 0:
-                    bridge.md.last_toot = statuses[0]["created_at"]
-
-            except (MastodonAPIError, MastodonNetworkError) as e:
-                l.error(e)
-                session.commit()
-                continue
-
-        if bridge.twitter_oauth_token:
-
-            try:
-                twitter_api = twitter.Api(
-                        consumer_key=c.TWITTER_CONSUMER_KEY,
-                        consumer_secret=c.TWITTER_CONSUMER_SECRET,
-                        access_token_key=bridge.twitter_oauth_token,
-                        access_token_secret=bridge.twitter_oauth_secret,
-                        tweet_mode='extended'  # Allow tweets longer than 140 raw characters
-                )
-                tl = twitter_api.GetUserTimeline()
-            except TwitterError as e:
-                l.error(e)
-                session.commit()
-                continue
-
-            else:
-                if len(tl) > 0:
-                    d = datetime.strptime(tl[0].created_at, '%a %b %d %H:%M:%S %z %Y')
-                    bridge.md.last_tweet = d
-
-        session.commit()
+    session.commit()
 
 session.close()
 db_connection.close()
