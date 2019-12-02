64df189d2173c44cd1e9b83f4127c61e39738e8e
James Moore <james@panic.com>
2019-12-01 16:39:08 -0800

    remove old worker_id column

--- moa/models.py
+++ moa/models.py
@@ -140,8 +140,6 @@ class Bridge(Base):
     t_settings_id = Column(Integer, ForeignKey('settings.id'), nullable=True)
     metadata_id = Column(Integer, ForeignKey('bridgemetadata.id'), nullable=True)
 
-    worker_id = Column(Integer, default=1)
-
     created = Column(DateTime, default=datetime.utcnow)
     updated = Column(DateTime)
 
